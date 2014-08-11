5f29c87ab597d26e4064828156eaa4350e1e183d
Jamie Strachan <jamie.strachan@gmail.com>
2014-08-10 23:43:33 -0400

    add utility functions to pq object

--- src/js/setup.js
+++ src/js/setup.js
@@ -1,1 +1,11 @@
 var pq = {};
+
+pq.utils = {};
+
+pq.utils.rnd = function (min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+pq.utils.randomFromList = function (list) {
+	return list[pq.utils.rnd(0, list.length - 1)];
+};
