3e128b6ab7f62b795c57d6d0642885656e440c66
Jamie Strachan <jamie.strachan@gmail.com>
2014-08-10 23:44:05 -0400

    add basic monster builder class

--- src/js/Monster.js
+++ src/js/Monster.js
@@ -1,10 +1,25 @@
-pq.Monster = function () {
+pq.Monster = function (hero, size) {
+	var names = ["Apone", "Hicks", "Hudson", "Frost", "Vasquez"];
+	var types = ["Space Marine", "Stopwatch", "Blerch"];
 
-	var private;
+	var name = pq.utils.randomFromList(names);
+	var type = pq.utils.randomFromList(types);
+	var level, hp;
 
-	var public;
+	if (size == "small") {
+		level = (hero.level > 1) ? hero.level - 1 : 1;
+	} else if (size == "large") {
+		level = hero.level + 1;
+	} else {
+		level = hero.level;
+	}
+
+	hp = level * 30;
 
 	return {
-		public: public
+		name: name,
+		type: type,
+		level: level,
+		hp: hp
 	};	
 };
