838843b08c35d850b52424f478391d41e54146f9
Jamie Strachan <jamie.strachan@gmail.com>
2014-08-11 01:25:04 -0400

    update state machine to have preswitch function, get quest -> battle transition to use real monsters

--- src/js/states.js
+++ src/js/states.js
@@ -4,8 +4,11 @@ pq.StateMachine = function (canvas, initState) {
 		state.render(canvas);
 		canvas.onclick = function (e) {
 			var nextState = state.react(e.target.id);
-			if (nextState && pq.states[nextState]) {
-				update(pq.states[nextState]);
+			if (nextState && pq.states[nextState.state]) {
+				if (nextState.preswitch) {
+					nextState.preswitch.apply(state);
+				}
+				update(pq.states[nextState.state]);
 			}
 		};
 	}
@@ -18,15 +21,15 @@ pq.StateMachine = function (canvas, initState) {
 };
 
 pq.State = function (prerender, template, postrender, nextStates) {
-	var view;
+	var model = {};
 
-	function render(container, model) {
+	function render(container) {
 		if (prerender) {
-			view = prerender(model);
+			model = prerender(model);
 		}
 
 		if (container) {
-			container.innerHTML = Mustache.render(template, view);
+			container.innerHTML = Mustache.render(template, model);
 		}
 
 		if (postrender) {
@@ -36,11 +39,16 @@ pq.State = function (prerender, template, postrender, nextStates) {
 
 	function react(eventId) {
 		if (nextStates[eventId]) {
-			return nextStates[eventId];
+			if (typeof nextStates[eventId] === "string") {
+				return { "state": nextStates[eventId] };
+			} else {
+				return nextStates[eventId];
+			}
 		}
 	}
 
 	return {
+		model: model,
 		render: render,
 		react: react
 	};
@@ -101,42 +109,47 @@ pq.states.startQuest = new pq.State(
 );
 pq.states.quest = new pq.State(
 	function (model) {
-		model.monster1 = new pq.Monster(pq.hero, "small");
-		model.monster2 = new pq.Monster(pq.hero, "medium");
-		model.monster3 = new pq.Monster(pq.hero, "large");
+		model.monster1 = new pq.Monster(pq.gameState.hero, "small");
+		model.monster2 = new pq.Monster(pq.gameState.hero, "medium");
+		model.monster3 = new pq.Monster(pq.gameState.hero, "large");
 		return model;
 	},
 	'<h2>Continue Your Quest</h2><ol><li><p>{{monster1.name}} the level {{monster1.level}} {{monster1.type}} ({{monster1.hp}} HP)</p><p><button id="battle1">Battle!</button></p></li><li><p>{{monster2.name}} the level {{monster2.level}} {{monster2.type}} ({{monster2.hp}} HP)</p><p><button id="battle2">Battle!</button></p></li><li><p>{{monster3.name}} the level {{monster3.level}} {{monster3.type}} ({{monster3.hp}} HP)</p><p><button id="battle3">Battle!</button></p></li></ol>',
 	null,
 	{
-		"battle1": "battle",
-		"battle2": "battle",
-		"battle3": "battle"
+		"battle1": { "state": "battle", "preswitch": function () { pq.gameState.monster = this.model.monster1; } },
+		"battle2": { "state": "battle", "preswitch": function () { pq.gameState.monster = this.model.monster2; } },
+		"battle3": { "state": "battle", "preswitch": function () { pq.gameState.monster = this.model.monster3; } },
 	}
 );
 pq.states.battle = new pq.State(
-	null,
-	'<h2>Battle!</h2><p>Enemy HP: <progress id="enemy" max="10" value="10"><span id="currenthp">10</span>/<span id="maxhp">10</span></progress></p><p>Hero AP: <progress id="hero" value="0"></progress></p><button id="victory">Victory!</button>',
+	function (model) {
+		model.hero = pq.gameState.hero;
+		model.monster = pq.gameState.monster;
+		return model;
+	},
+	'<h2>Battle!</h2><p>{{monster.name}} the level {{monster.level}} {{monster.type}}</p><p>Enemy HP: <progress id="enemy" max="{{monster.hp}}" value="{{monster.hp}}"></progress><span id="currenthp">{{monster.hp}}</span>/<span id="maxhp">{{monster.hp}}</span></p><p>Hero AP: <progress id="hero" value="0"></progress></p><button id="victory">Victory!</button>',
 	function () {
-		var updateInterval = 10;
+		var updateInterval = 50;
 		var enemyBar = document.getElementById("enemy");
 		var enemyHP = enemyBar.value;
 		var heroBar = document.getElementById("hero");
 		var heroAP = heroBar.value;
 		var victoryButton = document.getElementById("victory");
-		var damage, timer;
+		var damage, timer, start, then;
 
 		victoryButton.style.display = 'none';
 
 		function battle () {
 			if (!damage) {
-				damage = Math.floor(Math.random() * 3) + 1;
+				damage = pq.utils.rnd(pq.gameState.hero.hitMin, pq.gameState.hero.hitMax);
 				heroBar.max = damage;
 			}
 
 			if (heroAP >= damage) { // hit
 				enemyHP = enemyHP - damage;
 				enemyBar.value = enemyHP;
+				document.getElementById("currenthp").innerHTML = (enemyHP >= 0) ? enemyHP : 0;
 				heroAP = 0;
 				heroBar.value = heroAP;
 				damage = 0;
@@ -144,14 +157,20 @@ pq.states.battle = new pq.State(
 				if (enemyHP <= 0) {
 					clearInterval(timer);
 					victoryButton.style.display = '';
+					console.log(((new Date().getTime() - start) / 1000) + " seconds");
+				} else {
+					then = new Date().getTime();
 				}
 			} else { // charge
-				heroAP = heroAP + (updateInterval / 1000);
+				heroAP = heroAP + ((new Date().getTime() - then) / 1000);
 				heroBar.value = heroAP;
+				then = new Date().getTime();
 			}
 
 		}
 
+		start = new Date().getTime();
+		then = new Date().getTime();
 		timer = setInterval(battle, updateInterval);
 	},
 	{
