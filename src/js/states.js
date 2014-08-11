efb2dc9bcb645f1b28999872fc6b083b45337120
Jamie Strachan <jamie.strachan@gmail.com>
2014-08-10 23:45:14 -0400

    build out state machine with mustache, pre- and post- render functions

--- src/js/states.js
+++ src/js/states.js
@@ -17,14 +17,20 @@ pq.StateMachine = function (canvas, initState) {
 	};
 };
 
-pq.State = function (template, action, nextStates) {
+pq.State = function (prerender, template, postrender, nextStates) {
+	var view;
 
-	function render(container) {
-		if (container) {
-			container.innerHTML = template;
+	function render(container, model) {
+		if (prerender) {
+			view = prerender(model);
 		}
-		if (action) {
-			action();
+
+		if (container) {
+			container.innerHTML = Mustache.render(template, view);
+		}
+
+		if (postrender) {
+			postrender();
 		}
 	}
 
@@ -42,6 +48,7 @@ pq.State = function (template, action, nextStates) {
 
 pq.states = {};
 pq.states.menu = new pq.State(
+	null,
 	'<p><button id="startnew">Start New</button> <button id="continue">Continue</button></p>',
 	null,
 	{
@@ -50,6 +57,7 @@ pq.states.menu = new pq.State(
 	}
 );
 pq.states.intro = new pq.State(
+	null,
 	'<h2>Intro</h2><p><button id="continue">Continue</button></p>',
 	null,
 	{
@@ -57,6 +65,7 @@ pq.states.intro = new pq.State(
 	}
 );
 pq.states.createHero = new pq.State(
+	null,
 	'<h2>Create Your Hero</h2><p><button id="done">Done</button></p>',
 	null,
 	{
@@ -64,6 +73,7 @@ pq.states.createHero = new pq.State(
 	}
 );
 pq.states.activity = new pq.State(
+	null,
 	'<h2>Choose an Activity</h2><p><button id="createClass">Create a New Class</button> <button id="startQuest">Start a New Quest</button> <button id="continueQuest">Continue a Quest</button></p><p><button id="quit">Quit</button></p>',
 	null,
 	{
@@ -74,6 +84,7 @@ pq.states.activity = new pq.State(
 	}
 );
 pq.states.createClass = new pq.State(
+	null,
 	'<h2>Create a Class</h2><p><button id="done">Done</button></p>',
 	null,
 	{
@@ -81,6 +92,7 @@ pq.states.createClass = new pq.State(
 	}
 );
 pq.states.startQuest = new pq.State(
+	null,
 	'<h2>Start a Quest</h2><p><button id="done">Done</button></p>',
 	null,
 	{
@@ -88,7 +100,13 @@ pq.states.startQuest = new pq.State(
 	}
 );
 pq.states.quest = new pq.State(
-	'<h2>Continue Your Quest</h2><p><button id="battle1">Battle 1</button> <button id="battle2">Battle 2</button> <button id="battle3">Battle 3</button></p>',
+	function (model) {
+		model.monster1 = new pq.Monster(pq.hero, "small");
+		model.monster2 = new pq.Monster(pq.hero, "medium");
+		model.monster3 = new pq.Monster(pq.hero, "large");
+		return model;
+	},
+	'<h2>Continue Your Quest</h2><ol><li><p>{{monster1.name}} the level {{monster1.level}} {{monster1.type}} ({{monster1.hp}} HP)</p><p><button id="battle1">Battle!</button></p></li><li><p>{{monster2.name}} the level {{monster2.level}} {{monster2.type}} ({{monster2.hp}} HP)</p><p><button id="battle2">Battle!</button></p></li><li><p>{{monster3.name}} the level {{monster3.level}} {{monster3.type}} ({{monster3.hp}} HP)</p><p><button id="battle3">Battle!</button></p></li></ol>',
 	null,
 	{
 		"battle1": "battle",
@@ -97,6 +115,7 @@ pq.states.quest = new pq.State(
 	}
 );
 pq.states.battle = new pq.State(
+	null,
 	'<h2>Battle!</h2><p>Enemy HP: <progress id="enemy" max="10" value="10"><span id="currenthp">10</span>/<span id="maxhp">10</span></progress></p><p>Hero AP: <progress id="hero" value="0"></progress></p><button id="victory">Victory!</button>',
 	function () {
 		var updateInterval = 10;
@@ -140,6 +159,7 @@ pq.states.battle = new pq.State(
 	}
 );
 pq.states.result = new pq.State(
+	null,
 	'<h2>Victory!</h2><p>Loot!</p><p><button id="battle1">Battle 1</button> <button id="battle2">Battle 2</button> <button id="battle3">Battle 3</button></p><p><button id="rest">Rest</button></p>',
 	null,
 	{
